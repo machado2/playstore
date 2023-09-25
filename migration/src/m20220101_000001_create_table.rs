68ed9e0d43f7239df85d2c6aa7b9ecdb71214b1d
Fábio Machado de Oliveira <machado2@gmail.com>
2023-09-25 13:54:35 -0300

    temp

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -6,42 +6,56 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(App::Table)
                     .if_not_exists()
-                    .col(
-                        ColumnDef::new(Post::Id)
-                            .integer()
-                            .not_null()
-                            .auto_increment()
-                            .primary_key(),
+                    .col(ColumnDef::new(App::Id)
+                        .string()
+                        .not_null()
+                        .primary_key()
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
-                    .to_owned(),
+                    .col(ColumnDef::new(App::Data).json())
+                    .col(ColumnDef::new(App::LastCrawled).timestamp())
+                    .col(ColumnDef::new(App::LastUpdated).timestamp())
+                    .col(ColumnDef::new(App::LastSimilarSearch).timestamp())
+                    .col(ColumnDef::new(App::Priority)
+                        .integer()
+                        .default(0)
+                        .not_null()
+                    )
+                    .to_owned()
             )
-            .await
+            .await?;
+
+        // do the same for other models
+        // ...
+
+        Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
-            .await
+            .drop_table(Table::drop().table(App::Table).to_owned())
+            .await?;
+
+        // do the same for other models
+        // ...
+
+        Ok(())
     }
 }
 
 #[derive(DeriveIden)]
-enum Post {
+enum App {
     Table,
     Id,
-    Title,
-    Text,
+    Data,
+    LastCrawled,
+    LastUpdated,
+    LastSimilarSearch,
+    Priority,
 }
+
+// Add similar enum definitions for your `developer` and `category` models also
